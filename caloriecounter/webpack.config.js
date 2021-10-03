6fd9e66341215d568509f51366cf2a6458882377
OlPeep <evilmall@mail.ru>
2021-10-03 22:08:32 +0300

    babel/plugin-syntax-jsx

--- caloriecounter/webpack.config.js
+++ caloriecounter/webpack.config.js
@@ -61,7 +61,7 @@ module.exports = {
         options: {
           presets: [
             '@babel/preset-env', '@babel/preset-react',
-            { 'plugins': ['@babel/plugin-proposal-class-properties'] }
+            { 'plugins': ['@babel/plugin-proposal-class-properties', '@babel/plugin-syntax-jsx'] }
           ]
         }
       }
